ea526889bf3f0910059d82f56f837b9e48159bdd
none <litearc@yahoo.com>
2020-04-15 07:42:38 -0700

    more sidebar progress

--- src/comps/ed_tileset_sidebar.vue
+++ src/comps/ed_tileset_sidebar.vue
@@ -1,8 +1,10 @@
 <template lang='pug'>
 #ed_tileset_sidebar
-  #title.flex-row
-    span.bold Tileset Properties
-    .expand
+  .sb-title
+    span.bold.center TILESET PROPERTIES
+  #grid-tileset
+    .grid-head.bold Key
+    .grid-head.bold Value
     ui-tooltip(
       text='Add'
       placement='left'
@@ -11,7 +13,6 @@
         icon='plus'
         @click='on_tileset_plus'
       )
-  #grid
     template(v-for='(item,i) in tabs[itab].data.tileset_props')
       ui-input(v-model='item.key')
       ui-input(v-model='item.val')
@@ -25,18 +26,10 @@
           style='margin-left: 2px'
         )
   .divider
-  #title.flex-row
-    span.bold Tile Properties
-    .expand
-    ui-tooltip(
-      text='Add'
-      placement='left'
-    )
-      faicon.icon.clickable.title-icon(
-        icon='plus'
-        @click='on_tile_plus'
-      )
-  #grid
+  .sb-title
+    .inline-block.bold(style='width: 50%; text-align: center') TILE PROPERTIES
+    .inline-block.bold(style='width: 50%; text-align: center') TILE FLAGS
+  #grid-tile
     template(v-for='(item,i) in tabs[itab].data.tile_props')
       ui-input(v-model='item.key')
       ui-input(v-model='item.val')
@@ -52,6 +45,16 @@
 </template>
 
 <script>
+    // span.bold Tile Properties
+    // .expand
+    // ui-tooltip(
+    //   text='Add'
+    //   placement='left'
+    // )
+    //   faicon.icon.clickable.title-icon(
+    //     icon='plus'
+    //     @click='on_tile_plus'
+    //   )
 import { mapState, mapGetters, mapMutations } from 'vuex'
 
 export default
@@ -115,11 +118,14 @@ export default
   color: red
   /* border: 1px solid red */
 
-#grid
+#grid-tileset, #grid-tile
   display: grid
   grid-template-columns: 1fr 1fr 0fr
   margin-top: 8px
   grid-gap: 4px
   align-items: center
+
+  .grid-head
+    margin-left: 4px
 </style>
 
