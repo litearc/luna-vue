30646c1178e0ca5260faa64ea6b28cc24a043470
Haisam Islam <nimbuszero@gmail.com>
2020-05-06 20:14:16 -0700

    scaled image, cursor moves around

--- src/comps/ed_tileset_image.vue
+++ src/comps/ed_tileset_image.vue
@@ -18,7 +18,8 @@
 <script>
 import { mapState, mapGetters, mapMutations } from 'vuex'
 import * as PIXI from 'pixi.js'
-let app, grid
+let app, cursor
+let s = 2
 
 export default
 {
@@ -47,7 +48,9 @@ export default
   },
 
   methods: {
-    draw_grid(){
+    on_click(e){
+      cursor.x = s*this.tile_width*Math.floor(e.offsetX/(s*this.tile_width))
+      cursor.y = s*this.tile_height*Math.floor(e.offsetY/(s*this.tile_height))
     }
   },
 
@@ -74,23 +77,35 @@ export default
     let nx = Math.round(im_width/tile_width)
     let ny = Math.round(im_height/tile_height)
 
-    app = new PIXI.Application({width: im_width, height: im_height})
+    app = new PIXI.Application({width: s*im_width, height: s*im_height})
     this.$refs.root.appendChild(app.view)
     let tex = PIXI.Texture.fromBuffer(im_data, im_width, im_height)
     let spr = new PIXI.Sprite(tex)
+    spr.scale = {x:s, y:s} // does this need to be a PIXI.ObservablePoint?
+    app.view.addEventListener('mousedown', this.on_click)
 
     // tile grid
     let grid = new PIXI.Graphics()
     grid.position.set(0, 0)
     grid.lineStyle(1, 0xffffff)
     for (let ix = 1; ix < nx; ix++)
-      grid.moveTo(tile_width*ix, 0).lineTo(tile_width*ix, im_width+1)
+      grid.moveTo(s*tile_width*ix, 0).lineTo(s*tile_width*ix, s*im_height+1)
     for (let iy = 1; iy < ny; iy++)
-      grid.moveTo(0, tile_height*iy).lineTo(im_height+1, tile_height*iy)
+      grid.moveTo(0, s*tile_height*iy).lineTo(s*im_width+1, s*tile_height*iy)
     grid.alpha = .1
+
+    cursor = new PIXI.Graphics()
+    cursor.lineStyle(5, 0x000000)
+    cursor.beginFill(0xffffff, 0)
+    cursor.drawRect(0, 0, s*tile_width, s*tile_height)
+    cursor.lineStyle(3, 0x5c99d6)
+    cursor.beginFill(0xffffff, 0)
+    cursor.drawRect(0, 0, s*tile_width, s*tile_height)
+    cursor.endFill()
     
     app.stage.addChild(spr)
     app.stage.addChild(grid)
+    app.stage.addChild(cursor)
   },
 }
 </script>
