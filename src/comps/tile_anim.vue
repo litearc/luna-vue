283cc66714e5ec6437375324aa1ca876efa07ee4
Haisam Islam <nimbuszero@gmail.com>
2020-05-30 21:52:16 -0700

    layout progress

--- src/comps/tile_anim.vue
+++ src/comps/tile_anim.vue
@@ -2,14 +2,14 @@
 #tile_anim
   .flex-row.mt-8px.align-bl
     .w-1-2.flex-row.align-bl.pr-8px
-      span.mr-8px Type
+      span.bold.mr-8px Type
       ui-combobox.expand(
         :items='["Tiles", "Terra"]'
         :disabled='o.ianim === null'
         v-model='tile_type_model'
       )
     .w-1-2.flex-row.align-bl
-      span.ws-nowrap.mr-8px.ml-4px Frame duration
+      span.bold.ws-nowrap.mr-8px.ml-4px Frame duration
       ui-input.expand(
         small
         right
@@ -19,34 +19,27 @@
         v-model='frame_dur_model'
       )
       
-  //- .flex-row.mt-8px
-  div.mt-8px
-    // make the canvas zero-size so it doesn't expand the flex-row -
-    // we use the height of the flex-row to set the canvas dimensions
-    canvas.border-green.mr-8px(ref='canvas_preview' style='height:20px; width:80px; float:left')
-    .border-blue(ref='right' style='width:calc(100% - 88px); float:right')
-      canvas.full-w.border-red(ref='canvas_terra' style='height:20px')
-      .flex-row.full-w.mt-8px.border-green
-        #container.border-red.no-scrollbars(style='height:20px; overflow-x:scroll; overflow-y:hidden')
-          canvas.border-blue(ref='canvas_tiles' style='width:320px; height:20px')
+  .flex-row.mt-8px.border-gold(style='width:100%; position: relative; height: 120px')
+    .border-blue.mr-8px.fixed(style='width: 40px; height: 40px')
+    .border-red.expand(style='position: absolute; right: 0; left: 48px')
+      .span.bold Terra
+      .border-gold.expand.mb-8px.overflow-scroll.ws-nowrap(style='height: 20px')
+        span hello world this is a test to see whether this text will overflow and i can scroll to see more of it
+        //- .border-blue(style='height: 100%; width: 600px')
+      .flex-row.align-bl
+        span.bold Tiles
         .expand
         ui-tooltip(
           text='Remove'
           placement='left'
         )
-          faicon.icon.hover-hl.ml-8px(
+          faicon.icon.hover-hl(
             icon='minus'
             @click='on_minus'
           )
-      
-
-      //- .flex-row
-      //-   canvas.zero-size.expand(ref='canvas_terra')
-      //- .flex-row.full-w.align-center.mt-8px.border-red
-      //-   .border-blue.expand
-      //-   .border-green.fixed(style='height:10px; width:10px')
-
-  div(style='clear:both')
+      .border-gold.expand.overflow-scroll.ws-nowrap(style='height: 20px')
+        span hello world this is a test to see whether this text will overflow and i can scroll to see more of it
+        //- .border-blue(style='height: 100%; width: 600px')
 
   .flex-row.mt-8px
     .bold.ml-4px Anim
@@ -122,18 +115,11 @@ export default
     },
     nframes(){
       return (this.o.ianim === null) ? 0 : this.o.anims[this.o.ianim].tiles.length
-    }
+    },
   },
 
   watch: {
     nframes(v){
-      // console.log('changed number of tiles')
-      // console.log(o.g_tiles[0])
-      // app_tiles.stage.addChild(new PIXI.Sprite(o.g_tiles[0]))
-      // let i = o.anim_pos
-      // this.insert([tile_sprites, i,
-      //   new PIXI.Sprite(o.g_tiles[o.anims[o.ianim].tiles[i]])])
-      // app_preview.renderer.render(app_preview.stage)
     }
   },
 
@@ -162,7 +148,7 @@ export default
     },
     set_ianim(i){
       this.set_prop([o, 'ianim', i])
-    }
+    },
   },
 
   created(){
@@ -172,50 +158,6 @@ export default
   },
 
   mounted(){
-    let nanims = (o.ianim === null) ? 0 : o.anims[o.ianim].tiles.length
-    let nx = nanims*o.tile_w
-    let ny = o.tile_h
-    nx = 16, ny = 16
-    nx = 340
-    // this.$refs.canvas_tiles.style.width = `${nx}px`
-    // this.$refs.canvas_tiles.style.height = `${ny}px`
-    // this.$refs.canvas_terra.style.width = `${nx}px`
-    // this.$refs.canvas_terra.style.height = `${ny}px`
-
-    // app_terra = new PIXI.Application({
-    //   width: nx,
-    //   height: ny,
-    //   view: this.$refs.canvas_terra,
-    //   antialias: true,
-    //   backgroundColor: 0x1d1e1f,
-    // })
-
-    // app_tiles = new PIXI.Application({
-    //   width: nx,
-    //   height: ny,
-    //   view: this.$refs.canvas_tiles,
-    //   antialias: true,
-    //   backgroundColor: 0x1d1e1f,
-    // })
-
-    // o.anim_pos = nanims
-    // for (let i = 0; i < nanims; i++){
-    //   tile_sprites[i] = new PIXI.Sprite(o.g_tiles[o.anims[o.ianim].tiles[i]])
-    //   tile_sprites[i].x = o.tile_w*i
-    //   tile_sprites[i].y = 0
-    //   app_tiles.stage.addChild(tile_sprites[i])
-    // }
-
-    // let h = this.$refs.right.offsetHeight-1 // not sure why the -1 is needed
-    // this.$refs.canvas_preview.style.width = `${h}px`
-    // this.$refs.canvas_preview.style.height = `${h}px`
-    // app_preview = new PIXI.Application({
-    //   width: h,
-    //   height: h,
-    //   view: this.$refs.canvas_preview,
-    //   antialias: true,
-    //   backgroundColor: 0x1d1e1f,
-    // })
   }
 }
 </script>
